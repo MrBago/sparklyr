feef25f120d0b636866fbed646fa6f11bb935335
Bago Amirbekian <bago@databricks.com>
2018-12-09 23:42:08 -0800

    wip

--- R/databricks_connection.R
+++ R/databricks_connection.R
@@ -1,7 +1,7 @@
 databricks_connection <- function(config, extensions) {
   tryCatch({
     callSparkR <- get("callJStatic", envir = asNamespace("SparkR"))
-    # In Databricks notebooks, this variable is in the default namespace
+    # In Databricks environments (notebooks & rstudio) DATABRICKS_GUID is in the default namespace
     guid <- get("DATABRICKS_GUID", envir = .GlobalEnv)
     gatewayPort <- as.numeric(callSparkR("com.databricks.backend.daemon.driver.RDriverLocal",
                                          "startSparklyr",
@@ -39,7 +39,9 @@ new_databricks_connection <- function(scon, guid) {
     scon,
     subclass = "databricks_connection"
   )
-  rDriverLocal <- "com.databricks.backend.daemon.driver.RDriverLocal"
+  # In databricks, sparklyr should use the SqlContext associated with this guid to preserve the
+  # session & user isolation expected by users. Here we fetch the SqlContext for this guid to avoid
+  r_lriver_local <- "com.databricks.backend.daemon.driver.RDriverLocal"
   hive_context <- invoke_static(sc, rDriverLocal, "getDriver", guid) %>% invoke("sqlContext")
   sc$state$hive_context <- hive_context
   sc
